f0673acfaecec466f3eb763d85b6ab2c59ed2f59
joshi3 <joshi.naga3@gmail.com>
2020-01-11 18:39:02 +0000

    SystemUI: statusbar clock date customization (1/2)
    This commit add support for:
    
    - Clock enable/disable
    - Clock position
    - Clock seconds
    - AM/PM styles multilang support
    - Date format
    - Date style
    - Ref: https://github.com/Project-Xtended/frameworks_base/commit/82569c4c4b1a5cee5b0cf6411c4f34a8f5f963a0#diff-0946b3d5c1a161c78c4472659728905a

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -194,6 +194,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
+	mClockView.setQsHeader();
         mClockView.setClockHideableByUser(false);
         mDateView = findViewById(R.id.date);
 
@@ -343,6 +344,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
+	mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
     }
 
